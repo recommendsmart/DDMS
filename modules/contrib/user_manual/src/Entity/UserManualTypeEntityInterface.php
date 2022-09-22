6047076a73538ccc27d03c6a7173f1b7d8db6ffe
recommendsmart <recommendsmart1@gmail.com>
2022-09-22 22:35:51 +0530

    add some modules

---
+++ modules/contrib/user_manual/src/Entity/UserManualTypeEntityInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\user_manual\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Entity\EntityDescriptionInterface;
+
+/**
+ * Provides an interface for defining Practical entity type entities.
+ */
+interface UserManualTypeEntityInterface extends ConfigEntityInterface, EntityDescriptionInterface {}
